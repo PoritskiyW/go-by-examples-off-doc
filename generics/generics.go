27493162538f8a3373a1956451d60e45fb7ef439
Viacheslav Poritskiy <poritskiyw@gmail.com>
2024-06-16 21:14:20 +0300

    some more learning a bit random this time

---
+++ generics/generics.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func MapKeys[K comparable, V any](m map[K]V) []K {
+	r := make([]K, 0, len(m))
+	for k := range m {
+		r = append(r, k)
+	}
+	return r
+}
+
+type List[T any] struct {
+	head, tail *element[T]
+}
+
+type element[T any] struct {
+	next *element[T]
+	val  T
+}
+
+func (list *List[T]) Push(v T) {
+	if list.tail == nil {
+		list.head = &element[T]{val: v}
+		list.tail = list.head
+	} else {
+		list.tail.next = &element[T]{val: v}
+		list.tail = list.tail.next
+	}
+}
+
+func (list *List[T]) GetAll() []T {
+	var elems []T
+	for e := list.head; e != nil; e = e.next {
+		elems = append(elems, e.val)
+	}
+	return elems
+}
+
+func main() {
+	var m = map[int]string{1: "2", 2: "4", 4: "8"}
+
+	fmt.Println("keys:", MapKeys(m))
+
+	_ = MapKeys(m)
+
+	list := List[int]{}
+
+	list.Push(10)
+	list.Push(13)
+	list.Push(23)
+
+	fmt.Println("list:", list.GetAll())
+}
